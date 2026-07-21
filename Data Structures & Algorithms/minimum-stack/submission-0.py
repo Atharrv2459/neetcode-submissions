a12aea0874fa193954fe4db9ab4723d8ec0e65dc
Atharrv2459 <atharrv@gmail.com>
2026-07-21 10:53:35 +0530

    Bulk sync: 15 submissions

---
+++ Data Structures & Algorithms/minimum-stack/submission-0.py
@@ -0,0 +1,25 @@
+class MinStack:
+
+    def __init__(self):
+        self.stack = []
+
+        
+
+    def push(self, val: int) -> None:
+        self.stack.append(val)
+        return self.stack
+        
+
+    def pop(self) -> None:
+        self.stack.pop()
+        return self.stack
+        
+
+    def top(self) -> int:
+        return self.stack[-1]
+        
+
+    def getMin(self) -> int:
+        return min(self.stack)
+
+        
